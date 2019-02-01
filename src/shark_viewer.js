926b7a3ee5c07f087644ecc75a549c6598a80068
Jody Clements <jody@neomorphic.co.uk>
2019-02-01 13:58:56 -0500

    Minor changes to shaders and meshes.

--- src/shark_viewer.js
+++ src/shark_viewer.js
@@ -538,7 +538,7 @@ export default class SharkViewer {
         vertexShader: vertexShader,
         fragmentShader: fragmentShader,
         transparent: true,
-        alphaTest: 0.5 // if having transparency issues, try including: alphaTest: 0.5,
+        // alphaTest: 0.5 // if having transparency issues, try including: alphaTest: 0.5,
       });
       material.extensions.fragDepth = true;
 
@@ -829,7 +829,7 @@ export default class SharkViewer {
     for (const color in this.colors) {
       if (this.colors.hasOwnProperty(color)) {
         this.three_materials.push(
-          new THREE.MeshBasicMaterial({ color: this.colors[color] })
+          new THREE.MeshBasicMaterial({ color: this.colors[color], wireframe: false })
         );
       }
     }
